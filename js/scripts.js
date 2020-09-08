0a7bbe7e0fc341955df79e9c3e73ce801a569ae8
sloansobie <sloan.sobie@gmail.com>
2020-09-08 17:23:46 -0400

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,275 @@
+/*-----------------------------------------------------------------------------------
+
+    Theme Name: Buka
+    Theme URI: http://
+    Description: The Multi-Purpose Onepage Template
+    Author: UI-ThemeZ
+    Author URI: http://themeforest.net/user/UI-ThemeZ
+    Version: 1.0
+
+-----------------------------------------------------------------------------------*/
+
+
+$(function() {
+
+    "use strict";
+
+    var wind = $(window);
+
+/* ----------------------------------------------------------------
+                [ Navbar ( scrollIt ) ]
+-----------------------------------------------------------------*/
+
+    $.scrollIt({
+      upKey: 38,                // key code to navigate to the next section
+      downKey: 40,              // key code to navigate to the previous section
+      easing: 'swing',          // the easing function for animation
+      scrollTime: 600,          // how long (in ms) the animation takes
+      activeClass: 'active',    // class given to the active nav element
+      onPageChange: null,       // function(pageIndex) that is called when page is changed
+      topOffset: -80            // offste (in px) for fixed top navigation
+    });
+
+
+/* ----------------------------------------------------------------
+                [ Navbar ( Change Background & Logo ) ]
+-----------------------------------------------------------------*/
+
+    wind.on("scroll",function () {
+
+        var bodyScroll = wind.scrollTop(),
+            navbar = $(".navbar"),
+            logo = $(".navbar .logo> img");
+
+        if(bodyScroll > 100){
+
+            navbar.addClass("nav-scroll");
+            logo.attr('src', 'img/logo-dark.png');
+
+        }else{
+
+            navbar.removeClass("nav-scroll");
+            logo.attr('src', 'img/logo-light.png');
+        }
+    });
+
+
+    // close navbar-collapse when a  clicked
+    $(".navbar-nav a").on('click', function () {
+        $(".navbar-collapse").removeClass("show");
+    });
+
+
+/* ----------------------------------------------------------------
+                [ Progress Bar ]
+-----------------------------------------------------------------*/
+
+    wind.on('scroll', function () {
+        $(".skill-progress .progres").each(function () {
+            var bottom_of_object = 
+            $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = 
+            $(window).scrollTop() + $(window).height();
+            var myVal = $(this).attr('data-value');
+            if(bottom_of_window > bottom_of_object) {
+                $(this).css({
+                  width : myVal
+                });
+            }
+        });
+    });
+
+
+
+/* ----------------------------------------------------------------
+                [ Sections Background Image With Data ]
+-----------------------------------------------------------------*/
+
+    var pageSection = $(".bg-img, section");
+    pageSection.each(function(indx){
+        
+        if ($(this).attr("data-background")){
+            $(this).css("background-image", "url(" + $(this).data("background") + ")");
+        }
+    });
+
+
+/* ----------------------------------------------------------------
+                [ Owl-Carousel ]
+-----------------------------------------------------------------*/
+
+    // Testimonials owlCarousel
+    $('.carousel-single .owl-carousel').owlCarousel({
+        items:4,
+        loop:true,
+        margin: 0,
+        mouseDrag:true,
+        autoplay:true,
+        autoplayTimeout: 4000,
+        smartSpeed:2000
+    });
+
+
+
+/* ----------------------------------------------------------------
+                [ magnificPopup ]
+-----------------------------------------------------------------*/
+
+    $('.gallery').magnificPopup({
+        delegate: '.popimg',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+
+/* ----------------------------------------------------------------
+                [ countUp ]
+-----------------------------------------------------------------*/
+
+    $('.numbers .count').countUp({
+        delay: 10,
+        time: 1500
+    });
+
+/* ----------------------------------------------------------------
+                [ Accordian ]
+-----------------------------------------------------------------*/
+
+    var accordion = (function(){
+  
+      var $accordion = $('.js-accordion');
+      var $accordion_header = $accordion.find('.js-accordion-header');
+      var $accordion_item = $('.js-accordion-item');
+     
+      // default settings 
+      var settings = {
+        // animation speed
+        speed: 400,
+        
+        // close all other accordion items if true
+        oneOpen: false
+      };
+        
+      return {
+        // pass configurable object literal
+        init: function($settings) {
+          $accordion_header.on('click', function() {
+            accordion.toggle($(this));
+          });
+          
+          $.extend(settings, $settings); 
+          
+          // ensure only one accordion is active if oneOpen is true
+          if(settings.oneOpen && $('.js-accordion-item.active').length > 1) {
+            $('.js-accordion-item.active:not(:first)').removeClass('active');
+          }
+          
+          // reveal the active accordion bodies
+          $('.js-accordion-item.active').find('> .js-accordion-body').show();
+        },
+        toggle: function($this) {
+                
+          if(settings.oneOpen && $this[0] != $this.closest('.js-accordion').find('> .js-accordion-item.active > .js-accordion-header')[0]) {
+            $this.closest('.js-accordion')
+                   .find('> .js-accordion-item') 
+                   .removeClass('active')
+                   .find('.js-accordion-body')
+                   .slideUp()
+          }
+          
+          // show/hide the clicked accordion item
+          $this.closest('.js-accordion-item').toggleClass('active');
+          $this.next().stop().slideToggle(settings.speed);
+        }
+      }
+    })();
+
+$(document).ready(function(){
+  accordion.init({ speed: 300, oneOpen: true });
+});
+});
+
+
+// === window When Loading === //
+
+$(window).on("load",function (){
+
+    var wind = $(window);
+
+/* ----------------------------------------------------------------
+                [ Preloader ]
+-----------------------------------------------------------------*/    
+
+    $(".loading").addClass("loading-end").fadeOut(1000);
+
+
+/* ----------------------------------------------------------------
+                [ stellar ( Parallax ) ]
+-----------------------------------------------------------------*/
+
+    wind.stellar();
+
+
+/* ----------------------------------------------------------------
+                [ isotope Portfolio ( Masonery Style ) ]
+-----------------------------------------------------------------*/
+
+    $('.gallery').isotope({
+      // options
+      itemSelector: '.items'
+    });
+
+    var $gallery = $('.gallery').isotope({
+      // options
+    });
+
+    // filter items on button click
+    $('.filtering').on( 'click', 'span', function() {
+
+        var filterValue = $(this).attr('data-filter');
+
+        $gallery.isotope({ filter: filterValue });
+
+    });
+
+    $('.filtering').on( 'click', 'span', function() {
+
+        $(this).addClass('active').siblings().removeClass('active');
+
+    });
+    
+
+/* ----------------------------------------------------------------
+                [ contact form validator ]
+-----------------------------------------------------------------*/
+
+    $('#contact-form').validator();
+
+    $('#contact-form').on('submit', function (e) {
+        if (!e.isDefaultPrevented()) {
+            var url = "contact.php";
+
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).serialize(),
+                success: function (data)
+                {
+                    var messageAlert = 'alert-' + data.type;
+                    var messageText = data.message;
+
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    if (messageAlert && messageText) {
+                        $('#contact-form').find('.messages').html(alertBox);
+                        $('#contact-form')[0].reset();
+                    }
+                }
+            });
+            return false;
+        }
+    });
+
+});
+
